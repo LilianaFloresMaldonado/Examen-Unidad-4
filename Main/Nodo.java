0bcce61091e0f78089f59296d9806751c4b4e2ec
LilianaFloresMaldonado <71808888+LilianaFloresMaldonado@users.noreply.github.com>
2020-12-15 19:32:44 -0600

    Update Nodo.java

--- Main/Nodo.java
+++ Main/Nodo.java
@@ -1,20 +1,38 @@
 package Main;
-
-
 public class Nodo {
   int dato;
-     Nodo hijoIzq;
-     Nodo hijoDer;
-     String nombre;
-     public Nodo(int dato, String nom){
-         this.dato=dato;
-         this.nombre=nom;
-         this.hijoIzq=null;
-         this.hijoDer=null;
-     }
+  Nodo Hijoizquierdo, Hijoderecho;
+
+    public Nodo(int dat) {
+        this.dato = dat;
+        this.Hijoizquierdo = null;
+        this.Hijoderecho = null;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+    public Nodo getHijoIzq() {
+        return Hijoizquierdo;
+    }
+    public Nodo getHijoDer() {
+        return Hijoderecho;
+    }
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+    public void setHijoIzq(Nodo Hijoizquierdo) {
+        this.Hijoizquierdo = Hijoizquierdo;
+    }
+    public void setHijoDer(Nodo Hijoderecho) {
+        this.Hijoderecho = Hijoderecho;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + dato + "]";
+    }
+
+}
      
-     //Muestra la informacion completa del nodo
-     public String toString(){
-         return nombre+"Sus datos es "+dato;
-     }
- }  
+   
